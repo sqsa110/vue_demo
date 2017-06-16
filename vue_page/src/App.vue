62b8713777996ce461fc783c6bea6e7d83c18d33
sqsa110 <254750295@qq.com>
2017-06-16 11:43:15 +0800

    updata

---
+++ vue_page/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="aaa"></div>
+    <input type="text" v-model="aaa">
+    <hello :aaa="aaa" @bbb="bbb"></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      aaa: 'aaa'
+    }
+  },
+  methods: {
+    bbb (text) {
+      console.log('bbb事件触发了')
+      alert(text)
+    }
+  },
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import 'app.styl'
+</style>
+
---
+++ vue_page/src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ aaa }}</p>
+    <a href="javascript:;" @click="send_bbb">点击我发送一个bbb事件</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: {
+    aaa: {
+      type: String,
+      default: 'aaa2'
+    }
+  },
+  methods: {
+    send_bbb () {
+      this.$emit('bbb', '我发送了消息')
+    }
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
